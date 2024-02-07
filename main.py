bf29fb0e5cc5b9f8c1ea964321b6f3d7c76f73af
afternooon13 <afternooon13@gmail.com>
2024-02-07 20:21:03 +0900

    a

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 
 # Load data from data.csv
-data = pd.read_csv('data.csv')
+df = pd.read_csv('data.csv')
 
 # Display the data in a table
-st.write(data)
+edited_df = st.data_editor(df)
+st.write(df)
