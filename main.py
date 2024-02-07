b93ee34df3c5c4ba79fa96efb519cf511925c41b
afternooon13 <afternooon13@gmail.com>
2024-02-07 20:23:59 +0900

    save

--- main.py
+++ main.py
@@ -5,5 +5,8 @@ import pandas as pd
 df = pd.read_csv('data.csv')
 
 # Display the data in a table
-edited_df = st.data_editor(df)
-st.write(df)
+edited_df = st.data_editor(df) #編集可能形式での表の表示
+#st.write(df)
+
+if st.button('Save as data.csv'):
+    df.to_csv('data.csv', index=False)
