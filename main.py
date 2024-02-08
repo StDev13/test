2e58f2e35d8b4d713232a8363872a3412356a5b6
afternooon13 <afternooon13@gmail.com>
2024-02-08 10:09:39 +0900

    from gsheets

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import streamlit as st
 import pandas as pd
+from streamlit_gsheets import GSheetsConnection
 
-# Load data from data.csv
-df = pd.read_csv('data.csv')
 
+#あとはconnectionオブジェクトを作成してread()メソッド使うだけでpandsのdfになる
+conn = st.connection("gsheets", type=GSheetsConnection)
+df = conn.read()
 # Display the data in a table
-edited_df = st.data_editor(df) #編集可能形式での表の表示
+edited_df = st.data_editor(df) #編集可能形式での表の表示。ただしそのまま保存はできない(当たり前)
 #st.write(df)
 
 if st.button('Save as data.csv'):
-    df.to_csv('data.csv', index=False)
+    conn.write(df)
